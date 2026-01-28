1fe24ca855be1bf4873efc5935e447070dacb228
Mohamed Thalib <114242+thalib@users.noreply.github.com>
2026-01-28 16:04:32 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Moon Installation Script
+# Interactive installation script for Moon - Dynamic Headless Engine
+#
+
+set -e  # Exit on error
+
+# Check for root privileges
+check_root() {
+    print_info "Checking for root privileges..."
+    if [ "$EUID" -ne 0 ]; then
+        print_error "This script must be run as root."
+        echo ""
+        echo "Please run this script with sudo:"
+        echo "  sudo ./install.sh"
+        echo ""
+        exit 1
+    fi
+    print_success "Running with root privileges"
+}
+
+check_root
+
+echo 'sudo docker run --rm -v "$(pwd):/app" -v "$(pwd)/.gocache:/gocache" -w /app -e GOCACHE=/gocache golang:latest sh -c "go build -buildvcs=false -o moon ./cmd/moon"'
+
+sudo docker run --rm -v "$(pwd):/app" -v "$(pwd)/.gocache:/gocache" -w /app -e GOCACHE=/gocache golang:latest sh -c "go build -buildvcs=false -o moon ./cmd/moon"
